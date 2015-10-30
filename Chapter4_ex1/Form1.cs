0d06d638dd369822570d1224c13af734aa2a3f54
James Garfield <james.garfield1978@gmail.com>
2015-10-30 16:12:12 +0000

    Just started Lab1

---
+++ Chapter4_ex1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter4_ex1
+{
+    public partial class Form1 : Form
+    {
+        int startingMileage = 0;
+        int endingMileage = 0;
+        double milesTravelled = 0;
+        double reimburseRate = 0.39;
+        double amountOwed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            startingMileage = (int) nudStarting.Value;
+            endingMileage = (int) nudEnding.Value;
+
+            if (startingMileage >= endingMileage)
+            {
+                MessageBox.Show("The starting mileage must be less than the ending mileage", "Cannot calculate mileage");
+            }
+            else
+            {
+                milesTravelled = endingMileage - startingMileage;
+                amountOwed = milesTravelled * reimburseRate;
+                lblAmount.Text = "€" + amountOwed;
+            }
+        }
+    }
+}
