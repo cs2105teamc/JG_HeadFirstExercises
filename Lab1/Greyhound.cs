0c1ec7cb6430a65fab756a20d20da6822af4a4ce
James Garfield <james.garfield1978@gmail.com>
2015-11-01 12:00:06 +0000

    Completed Lab 1!! :)

---
+++ Lab1/Greyhound.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public class Greyhound
+    {
+        public int startingPosition;
+        public int raceTrackLength;
+        public PictureBox myPictureBox;
+        public int location = 0;
+        public static readonly Random randomiser = new Random();
+
+        public Greyhound(PictureBox myPB, int RaceTrackLength)
+        {
+            myPictureBox = myPB;
+            startingPosition = myPB.Left;
+            raceTrackLength = RaceTrackLength;
+        }
+
+        public bool Run()
+        {
+            //randomiser = new Random();
+            //update postition            
+            location += randomiser.Next(1, 4);
+            myPictureBox.Left = startingPosition + location ;
+            // if I won
+            if (myPictureBox.Left >= 852)           
+                return true;            
+            else
+                return false;
+        }
+
+        public void TakeStartingPosition()
+        {
+            //reset position to 0
+            location = 0;
+            myPictureBox.Left = startingPosition;
+        }
+    }
+}
---
+++ Lab1/Guy.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public class Guy
+    {
+        public string name;
+        public Bet myBet;
+        public int cash;
+
+        public RadioButton myRadioButton;
+        public Label myLabel;
+
+        public Guy(string Name, int Cash, RadioButton MyRb, Label MyLabel)
+        {
+            name = Name;
+            cash = Cash;
+            myRadioButton = MyRb;
+            myLabel = MyLabel;
+            myBet = new Bet(this);
+            this.UpdateLabels();
+        }
+
+        public void UpdateLabels()
+        {
+            // set label to my bet and cash
+            myRadioButton.Text = name + " has €" + cash;            
+            myLabel.Text = myBet.GetDescription();
+        }
+
+        public void ClearBet()
+        {
+            myBet = new Bet(this);
+            UpdateLabels();
+        }
+
+        public bool PlaceBet(int BetAmount, int DogToWin)
+        {
+            if (BetAmount <= cash)
+            {
+                myBet = new Bet(BetAmount, DogToWin, this);
+                UpdateLabels();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Collect(int Winner)
+        {
+            //Pay out           
+            cash += myBet.PayOut(Winner);
+
+            //clear bet
+            this.ClearBet();
+
+            //update labels
+            this.UpdateLabels();
+        }
+
+    }
+}
